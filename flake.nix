206ef9fb5a8386246d9467be0a5b0e3871304cf8
bla <bla>
2025-11-16 18:02:33 +0100

    merge cleanup

--- flake.nix
+++ flake.nix
@@ -7,80 +7,63 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (system:
         let pkgs = import nixpkgs { inherit system; };
         python = pkgs.python312;
         in {
-          # Dev shell: interactive environment
-          devShell = pkgs.mkShell {
-            name = "gng_py build";
-            buildInputs = with pkgs; [
-              rustc
-              cargo
-              maturin
-              python
-              vscodium
-              python.pkgs.jupyterlab
-              python.pkgs.jupyter
-              python.pkgs.ipykernel
-              python.pkgs.notebook
-              python.pkgs.numpy
-              python.pkgs.pandas  
-              python.pkgs.matplotlib
-            ];
+# Dev shell: interactive environment
+        devShell = pkgs.mkShell {
+        name = "gng_py build";
+        buildInputs = with pkgs; [
+        rustc
+        cargo
+        maturin
+        python
+        vscodium
+        gcc
+        python.pkgs.jupyterlab
+        python.pkgs.jupyter
+        python.pkgs.ipykernel
+        python.pkgs.notebook
+        python.pkgs.numpy
+        python.pkgs.pandas  
+        python.pkgs.matplotlib
+        ];
 
- # Make sure the Python packages in this shell are visible to venvs
-  shellHook = ''
-    export PYTHONPATH=${python.sitePackages}
-    echo "Tip: Now run 'python -m venv .venv && source .venv/bin/activate'"
-    echo "Then you can 'pip install dist/*.whl' after building with maturin."
-    '';
-
-
-
+# Make sure the Python packages in this shell are visible to venvs
+        shellHook = ''
+          export PYTHONPATH=${python.sitePackages}
+        echo "Tip: Now run 'python -m venv .venv && source .venv/bin/activate'"
+          echo "Then you can 'pip install dist/*.whl' after building with maturin."
+          '';
 
 
+        };
 
+# Declarative package: this is what nix build uses
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "gng_py";
+          version = "0.1.1";
+          src = ./gng_py;
 
-
-
-
-
-
-
-
-
-
-
-
+# Prefetch crates according to Cargo.lock
+          cargoLock = {
+            lockFile = ./gng_py/Cargo.lock;
           };
 
-          # Declarative package: this is what nix build uses
-          packages.default = pkgs.rustPlatform.buildRustPackage {
-            pname = "gng_py";
-            version = "0.1.1";
-            src = ./gng_py;
+# Add Python & Maturin for building the wheel
+          nativeBuildInputs = [ python pkgs.maturin ];
 
-            # Prefetch crates according to Cargo.lock
-            cargoLock = {
-              lockFile = ./gng_py/Cargo.lock;
-            };
+          buildPhase = ''
+            echo "Building gng_py with maturin..."
+            maturin build --release --locked -o dist --skip-auditwheel
+            '';
 
-            # Add Python & Maturin for building the wheel
-            nativeBuildInputs = [ python pkgs.maturin ];
-
-            buildPhase = ''
-              echo "Building gng_py with maturin..."
-              maturin build --release --locked -o dist --skip-auditwheel
-              '';
-
-            installPhase = ''
-              mkdir -p $out/dist
-              cp dist/*.whl $out/dist/
-                       '';
-          };
-        }
-  );
-}
-#maturin build --release --locked -o dist manylinux2017
-#maturin build --release --locked -o dist
+          installPhase = ''
+            mkdir -p $out/dist
+            cp dist/*.whl $out/dist/
+                     '';
+                     };
+                     }
+                     );
+                     }
