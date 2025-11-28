e04329ca43feeacee36189c469ffeee344e55412
bla <bla>
2025-11-28 21:05:34 +0100

    examples and tools remove

--- examples/run_rust_example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cwd=$PWD
-cd ${cwd}/../
-
-nix-shell --run "cd examples/test_app_rust && \
-                 cargo clean && \
-                 cargo build --release && \
-                 cargo run  --release"
