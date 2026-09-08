3d74a8db82a1940afb22f96d54c859934570f76f
Falko Duwanoff <f.duwanoff@gmail.com>
2026-09-08 21:26:29 +0200

    remove obsolete files

--- scripts/5-clean_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cwd=$PWD
-
-# Build Rust source
-
-nix develop --command bash -c "cd gng && \
-                               cargo clean"
-
--- scripts/container/3-install_gng.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker exec -it pytorch_project_cont fish -c "pip install /workDir/gng/target/wheels/*"
-
